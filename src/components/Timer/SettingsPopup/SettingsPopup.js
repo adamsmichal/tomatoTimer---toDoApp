70ec05cc4b949889e9dca912cd64908eb8a6e26f
Michał Adamski <adamsmichal@wp.pl>
2020-02-19 14:51:49 +0100

    add popup style

--- src/components/Timer/SettingsPopup/SettingsPopup.js
+++ src/components/Timer/SettingsPopup/SettingsPopup.js
@@ -3,20 +3,20 @@ import React from 'react';
 const SettingsPopup = (props) => {
 
     let content = (
-        <React.Fragment>
-            <div>
+        <div className={'settings-popup'}>
+            <div className={'settings-popup__item'}>
                 <button onClick={props.subHr}>-</button>Hours: {props.hr}<button onClick={props.addHr}>+</button>
             </div>
-            <div>
+            <div className={'settings-popup__item'}>
                 <button onClick={props.subMin}>-</button>Minute: {props.min}<button onClick={props.addMin}>+</button>
             </div>
-            <div>
+            <div className={'settings-popup__item'}>
                 <button onClick={props.subShortBreak}>-</button>Short break: {props.shortBreak} min<button onClick={props.addShortBreak}>+</button>
             </div>
-            <div>
+            <div className={'settings-popup__item'}>
                 <button onClick={props.subLongBreak}>-</button>Long break: {props.longBreak} min<button onClick={props.addLongBreak}>+</button>
             </div>
-        </React.Fragment>
+        </div>
     )
 
     if (!props.isOpen) {
