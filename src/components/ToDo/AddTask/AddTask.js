08122f9ea85c13b20c6412d65efe7e791c0f733b
Michał Adamski <adamsmichal@wp.pl>
2020-02-20 20:32:43 +0100

    add buttons animations

--- src/components/ToDo/AddTask/AddTask.js
+++ src/components/ToDo/AddTask/AddTask.js
@@ -32,7 +32,7 @@ class AddTask extends Component {
     render() { 
         return (
             <div className="to-do-form">
-                <input className={'to-do-form__text'} type="text" placeholder="dodaj zadanie" value={this.state.text} onChange={this.handleText}/>
+                <input className={'to-do-form__text'} type="text" placeholder="add task" value={this.state.text} onChange={this.handleText}/>
                 <input className={'to-do-form__check'} type="checkbox" id='important' checked={this.state.checked} onChange={this.handleCheckbox}/>
                 <button className={'to-do-form__btn'} onClick={this.handleAddClick}>+</button>
             </div>
