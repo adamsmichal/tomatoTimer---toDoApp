75210921c198a8c0c3abd2ec138a3889a7f19eaf
Michał Adamski <adamsmichal@wp.pl>
2020-02-22 00:58:34 +0100

    add css style for instruction popup

--- src/components/Header/Instruction/Instruction.js
+++ src/components/Header/Instruction/Instruction.js
@@ -6,12 +6,15 @@ const Instruction = (props) => {
         <div className={'instruction'}>
             <ul className={'instruction__list'}>
                 <h2 className={'instruction__title'}>Instruction</h2>
-                <li>To add task you can press button or enter</li>
+                <li>To add task you can press button or hit enter</li>
+                <hr/>
                 <li>To add the priority task, check fire icon next to text field</li>
+                <hr/>
                 <li>You will get an extra minute to break time for each completed priority task</li>
+                <hr/>
                 <li>Next to tasks you will find a timer that shows you when the task has been added or finished</li>
             </ul>
-            <button className={'instruction__btn'} onClick={props.closeInstructionPopup}>X</button>
+            <button className={'functional-btn instruction__btn'} onClick={props.closeInstructionPopup}>Close</button>
         </div>
     )
 
