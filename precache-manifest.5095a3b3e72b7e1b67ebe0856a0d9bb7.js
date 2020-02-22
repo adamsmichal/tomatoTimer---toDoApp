b5411f9775c9f4d5b8b3e72f481084edbb034929
Michał Adamski <adamsmichal@wp.pl>
2020-02-22 12:32:25 +0100

    Updates

---
+++ precache-manifest.5095a3b3e72b7e1b67ebe0856a0d9bb7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36df6f0218d15b5dceafe014acfdd9c",
+    "url": "/tomatoTimer-toDoApp/index.html"
+  },
+  {
+    "revision": "7be28e08dc14d63e3f48",
+    "url": "/tomatoTimer-toDoApp/static/css/main.4ef5c90a.chunk.css"
+  },
+  {
+    "revision": "5c5e02f65fce84178f0a",
+    "url": "/tomatoTimer-toDoApp/static/js/2.107f1ddb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tomatoTimer-toDoApp/static/js/2.107f1ddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be28e08dc14d63e3f48",
+    "url": "/tomatoTimer-toDoApp/static/js/main.2a3e65bc.chunk.js"
+  },
+  {
+    "revision": "5ce9c888ec769fcda80c",
+    "url": "/tomatoTimer-toDoApp/static/js/runtime-main.862803fc.js"
+  },
+  {
+    "revision": "c3c23cdd8d76195a82fa28acec15c80e",
+    "url": "/tomatoTimer-toDoApp/static/media/fire-solid.c3c23cdd.svg"
+  },
+  {
+    "revision": "17861ffead670d4056c589854f7e795c",
+    "url": "/tomatoTimer-toDoApp/static/media/info-solid.17861ffe.svg"
+  },
+  {
+    "revision": "7726bd8eda5de1f9bf98669c012f6012",
+    "url": "/tomatoTimer-toDoApp/static/media/niceSound.7726bd8e.wav"
+  }
+]);
